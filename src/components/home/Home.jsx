9941638561e3fdb4162ce7ec7d32883e25ed78c0
Ashton607 <portmanashton4@gmail.com>
2026-05-11 16:50:09 +0200

    added animations

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
 import Lux from '../../assets/Lux logo.svg'
 
@@ -8,12 +8,12 @@ const Home = () => {
       
 
       <div className="container">
-        <img src={Lux} alt="Lux Logo" />
-        <h1 className='title'>Welcome to Lux</h1>
-        <p>Bringing natural elegance to your digital experience</p>
+        <img src={Lux} alt="Lux Logo" className='animate-in' />
+        <h1 className='title animate-in'>Welcome to Lux</h1>
+        <p className='animate-in'>Bringing natural elegance to your digital experience</p>
 
-          <button>Get Started</button>
-          <button>Learn More</button>
+          <button className='animate-in'>Get Started</button>
+          <button className='animate-in'>Learn More</button>
         </div>
       
       
