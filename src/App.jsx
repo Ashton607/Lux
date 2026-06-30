9724de0702a6670ef9087143cf5809ca94e6b6ef
Ashton607 <portmanashton4@gmail.com>
2026-06-30 20:52:56 +0200

    Changed hash router to browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/home/Home'
 import Dashboard from './components/dashboard/Dashboard'
 
