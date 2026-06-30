c9c085d8712f826cda978f826a5c90e01f578c80
Ashton607 <portmanashton4@gmail.com>
2026-06-30 21:21:47 +0200

    Added vite plugin sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+  react(),
+  Sitemap({
+      hostname: 'https://luxinteractive.co.za', 
+      dynamicRoutes: [
+        '/dashboard',
+      ]
+    })
+  ],
   base: '/',
 })
